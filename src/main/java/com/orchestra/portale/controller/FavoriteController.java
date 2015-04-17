c62fa976c2d026ba41bfbeb3f67cf8294808b4ee
antocaso <antocaso@gmail.com>
2015-04-17 15:24:46 +0200

    Gestione Preferiti

---
+++ src/main/java/com/orchestra/portale/controller/FavoriteController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orchestra.portale.controller;
+
+import com.orchestra.portale.dbManager.PersistenceManager;
+import com.orchestra.portale.persistence.sql.entities.Favorite;
+import com.orchestra.portale.persistence.sql.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author antonio
+ */
+@Controller
+public class FavoriteController {
+    //Manager della persistenza
+    @Autowired
+    PersistenceManager pm;
+
+    @RequestMapping(value = "/saveFavorite", method = RequestMethod.GET)
+    public @ResponseBody
+    String saveFavorite(@RequestParam String id_user, @RequestParam String id_poi) {
+        Favorite favorite = new Favorite();
+        favorite.setIdUser(Integer.parseInt(id_user));
+        favorite.setIdPoi(id_poi);
+        pm.saveFavorite(favorite);
+        
+        return "ok";
+    }    
+    
+
+    @RequestMapping(value = "/saveFavoriteRating", method = RequestMethod.GET)
+    public @ResponseBody
+    String saveFavoriteRating(@RequestParam String id_user, @RequestParam String id_poi, @RequestParam String rating) {
+    
+        pm.updateFavoriteRating(Integer.parseInt(rating), Integer.parseInt(id_user), id_poi);
+        
+        return "ok";
+    }        
+
+
+    @RequestMapping(value = "/getFavorites", method = RequestMethod.GET)
+    public @ResponseBody
+    String getFavorites(@RequestParam String id_user) {
+        
+        pm.findFavoritesByIdUser(Integer.parseInt(id_user));
+        
+        return "ok";
+    }        
+    
+}
---
+++ src/main/java/com/orchestra/portale/persistence/sql/entities/Favorite.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orchestra.portale.persistence.sql.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author antonio
+ */
+@Entity
+@Table(name = "favorite")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Favorite.findAll", query = "SELECT f FROM Favorite f"),
+    @NamedQuery(name = "Favorite.findById", query = "SELECT f FROM Favorite f WHERE f.id = :id"),
+    @NamedQuery(name = "Favorite.findByIdUser", query = "SELECT f FROM Favorite f WHERE f.idUser = :idUser"),
+    @NamedQuery(name = "Favorite.findByIdPoi", query = "SELECT f FROM Favorite f WHERE f.idPoi = :idPoi"),
+    @NamedQuery(name = "Favorite.findByRating", query = "SELECT f FROM Favorite f WHERE f.rating = :rating")})
+public class Favorite implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_user")
+    private int idUser;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "id_poi")
+    private String idPoi;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "rating")
+    private int rating;
+
+    public Favorite() {
+    }
+
+    public Favorite(Integer id) {
+        this.id = id;
+    }
+
+    public Favorite(Integer id, int idUser, String idPoi, int rating) {
+        this.id = id;
+        this.idUser = idUser;
+        this.idPoi = idPoi;
+        this.rating = rating;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getIdPoi() {
+        return idPoi;
+    }
+
+    public void setIdPoi(String idPoi) {
+        this.idPoi = idPoi;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Favorite)) {
+            return false;
+        }
+        Favorite other = (Favorite) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.orchestra.portale.persistence.sql.entities.Favorite[ id=" + id + " ]";
+    }
+    
+}
