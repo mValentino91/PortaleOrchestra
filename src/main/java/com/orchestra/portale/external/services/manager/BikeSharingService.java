a6b488a571f28ada5f6e5ce5cbdb76aff1c6d842
Marco Valentino <Marco Valentino@100.101.38.3>
2015-05-05 11:31:54 +0200

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/jsp/components/sideBar.jsp
    	src/main/webapp/dist/img/logoOrchestra.png

--- src/main/java/com/orchestra/portale/external/services/manager/BikeSharingService.java
+++ src/main/java/com/orchestra/portale/external/services/manager/BikeSharingService.java
@@ -27,7 +27,7 @@ public class BikeSharingService implements ExternalServiceManager {
     public BikeSharingService(PersistenceManager manager) {
         pm = manager;
     }
-    
+
     @Override
     public String load() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -36,20 +36,19 @@ public class BikeSharingService implements ExternalServiceManager {
     @Override
     public String getResponse(Map<String, String[]> mapParams) {
         try {
-            
+
             HttpURLConnection urlConnection = (HttpURLConnection) new URL(baseUrl).openConnection();
             urlConnection.setConnectTimeout(15000);
             urlConnection.setReadTimeout(30000);
-
-            urlConnection.addRequestProperty("Accept-Language", Locale.getDefault().toString().replace('_', '-'));
-
+            urlConnection.addRequestProperty("Accept-Language", "it-IT");
+            urlConnection.addRequestProperty("Accept", "application/json");
             String result = IOUtils.toString(urlConnection.getInputStream());
             urlConnection.disconnect();
 
             return result;
-            
+
         } catch (IOException e) {
-            return "response{code:1,error:" + e.getMessage() + "}";
+            return "response:{code:1,error:" + e.getMessage() + "}";
         }
     }
 }
